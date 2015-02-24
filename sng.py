4bd321eaf7987d2651cc8e449165d1a4f31e9883
vinylplz <greg.forties@gmail.com>
2015-02-23 18:49:22 -0600

    forground colors and style fixes

--- sng.py
+++ sng.py
@@ -3,22 +3,28 @@ import sys
 
 import argparse
 
-from colorama import init, Back
+from colorama import init, Back, Fore
 
 from random import randrange
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--numnames', action='store', dest='numnames', type=int, default=15)
-    parser.add_argument('-W', '--wordfile', action='store', dest='wordfile', default=None)
-    parser.add_argument('-T', '--templatefile', action='store', dest='templatefile', default=None)
+    parser.add_argument('-n', '--numnames', action='store', dest='numnames',
+            type=int, default=15)
+    parser.add_argument('-W', '--wordfile', action='store', dest='wordfile',
+            default=None)
+    parser.add_argument('-T', '--templatefile', action='store',
+            dest='templatefile', default=None)
     parsed = parser.parse_args()
     main(parsed)
     return
 
+
 class SongNameGenerator(object):
     """Generates song names from a list of words"""
-    def __init__(self,wordlist=[],templates=[],wordfile=None,templatefile=None):
+    def __init__(self, wordlist=[], templates=[], wordfile=None,
+                 templatefile=None):
         super(SongNameGenerator, self).__init__()
         self._wordlist = wordlist
         self._templates = templates
@@ -38,7 +44,7 @@ class SongNameGenerator(object):
         with open(wordfile) as wf:
             words = wf.readlines()
             self.add_words(words)
-    
+
     def add_template(self, template):
         self._templates.append(template.rstrip())
 
@@ -52,33 +58,35 @@ class SongNameGenerator(object):
             self.add_templates(templates)
 
     def generate_name(self):
-        if len(self._templates) == 0 :
+        if len(self._templates) == 0:
             print "ERROR: No templates loaded"
             sys.exit(1)
-        if len(self._wordlist) == 0 :
+        if len(self._wordlist) == 0:
             print "ERROR: No words loaded"
             sys.exit(1)
 
         template = self._get_random_template()
-        return template.format(self._get_random_word(),self._get_random_word())
-        
+        return template.format(self._get_random_word(),
+                               self._get_random_word())
+
     def _get_random_word(self):
-        return self._wordlist[randrange(0,len(self._wordlist))]
+        return self._wordlist[randrange(0, len(self._wordlist))]
 
     def _get_random_template(self):
-        return self._templates[randrange(0,len(self._templates))]
-        
+        return self._templates[randrange(0, len(self._templates))]
+
 
 def main(parsed):
     init()
-    colors = [Back.GREEN, Back.YELLOW, Back.RED]
+    bg_colors = [Back.GREEN, Back.YELLOW, Back.RED]
+    fg_colors = [Fore.BLACK, Fore.BLACK, Fore.YELLOW]
     numnames = getattr(parsed, 'numnames')
     wordfile = getattr(parsed, 'wordfile')
     templatefile = getattr(parsed, 'templatefile')
     generator = SongNameGenerator()
 
     if wordfile:
-        generator.add_words_from_file(getattr(parsed,'wordfile'))
+        generator.add_words_from_file(getattr(parsed, 'wordfile'))
     else:
         try:
             generator.add_words_from_file('wordlist.txt')
@@ -93,9 +101,10 @@ def main(parsed):
         except:
             pass
 
-    for i in range(0,numnames):
+    for i in range(0, numnames):
         name = generator.generate_name()
-        print colors[i % len(colors)], name.upper() ,Back.RESET
+        print bg_colors[i % len(bg_colors)], fg_colors[i % len(fg_colors)], \
+              name.upper(), Back.RESET, Fore.RESET
     return
 
 if __name__ == '__main__':
